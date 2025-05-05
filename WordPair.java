8d3ae657c57d54a846e575918ba47f9089b34d8a
socratest3 <socratest3@nycstudents.net>
2025-05-05 19:08:41 -0400

    Update WordPair.java

--- WordPair.java
+++ WordPair.java
@@ -1,1 +1,23 @@
+public class WordPair 
+{
+    /** Constructs a WordPair object. */
+    public WordPair(String first, String second) 
+  {
+       }
 
+    /** Returns the first string of this WordPair object. */
+    public String getFirst() 
+  {
+        
+        }
+
+    /** Returns the second string of this WordPair object. */
+    public String getSecond() 
+  {
+    
+    }
+    public String toString()
+  {
+        return "(\"" + first + "\", \"" + second + "\")";
+    }
+}
